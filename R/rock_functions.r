c5f47ddfffef1eaa6636a02422275a35f5078bec
psmits <thestinkypete@gmail.com>
2017-10-05 15:20:35 -0700

    working on synonymizing or cutting specific words

--- R/rock_functions.r
+++ R/rock_functions.r
@@ -11,6 +11,7 @@ lith.matrix <- function(lit) {
     rock.matrix[ii, mm] <- lit[[ii]][, 2]
     rock.matrix[ii, mm]
   }
+  rock.freq <- table(Reduce(c, llply(lit, function(x) x[, 1])))
   bcomp <- match(names(which.max(rock.freq)), colnames(rock.matrix))
   rock.matrix[, c(1, bcomp)] <- rock.matrix[, c(bcomp, 1)]
   rock.matrix
@@ -23,5 +24,25 @@ wordrep <- function(dec, wordd) {
                        w <- x %in% wordd
                        x[w] <- wordd[1]
                        x}))
+  #out <- llply(out, function(y) laply(y, function(x) paste0(x, collapse = ' ')))
   out
+}
+
+# remove individual words
+wordrm <- function(dec, words) {
+  aa <- dec
+  bb <- llply(dec, function(y) llply(y, function(x) x %in% words))
+  aa <- Map(function(a, b) Map(function(x, y) x[!y], a, b), 
+                   aa , bb)
+  aa
+}
+
+
+# remove duplicated words
+worddup <- function(dec) {
+  ndup <- llply(dec, function(y) llply(y, function(x) !duplicated(x)))
+  out <- Map(function(a, b) Map(function(x, y) x[y], a, b), dec, ndup)
+  out <- llply(out, function(x) 
+                     laply(x, function(y) paste0(y, collapse = ' ')))
+  out 
 }
--- R/rock_mung.r
+++ R/rock_mung.r
@@ -1,7 +1,8 @@
 library(plyr)
 library(stringr)
 library(compositions)
-library(glmnet)
+#library(glmnet)
+#library(rpart)
 
 source('download_scrap.r')
 source('rock_functions.r')
@@ -10,6 +11,7 @@ source('rock_functions.r')
 rock.fossil <- data.frame(nocc = strat.ord$pbdb_occurrences, 
                           ncol = strat.ord$pbdb_collections)
 rock.fossil$yesno <- (rock.fossil$ncol > 0) * 1
+rownames(rock.fossil) <- strat.ord$unit_id
 
 # given data call, want to extract rock covariates
 # thickness
@@ -23,22 +25,7 @@ strat.ord[weird, tt] <- strat.ord[weird, rev(tt)]
 lit <- laply(strat.ord$lith, function(x) str_split(x, '\\|'))
 lit <- llply(lit, str_trim)
 lit <- llply(lit, function(x) str_split(x, '  ~ ', simplify = TRUE))
-# get rid of duplicated words because that's stupid
-lit.words <- llply(lit, function(x) str_split(x[, 1], ' '))
-ndup <- llply(lit.words, function(x) llply(x, function(y) !duplicated(y)))
-lit.words <- Map(function(a, b) Map(function(x, y) x[y], a, b), lit.words, ndup)
-lit.words <- llply(lit.words, function(x) 
-                   laply(x, function(y) paste0(y, collapse = ' ')))
-lit <- Map(function(x, y) {x[, 1] <- y; x}, lit, lit.words)
 names(lit) <- strat.ord$unit_id
-# some lithologies appear/counted twice which makes no sense
-
-# break up the words for fun and profit
-rock.freq <- table(Reduce(c, llply(lit, function(x) x[, 1])))
-rock.desc <- unique(Reduce(c, llply(lit, function(x) x[, 1])))
-rock.words <- Reduce(c, str_split(rock.desc, ' '))
-rock.word <- unique(rock.words)
-rock.wordfreq <- table(rock.words)
 
 # cull unhelpful descriptions
 bad <- c('igneous', 'volcanic', 'metamorphic', 'chemical',
@@ -50,17 +37,60 @@ lit <- lit[laply(lit, nrow) > 0]
 
 # synonymize some words
 dec <- llply(lit, function(x) str_split(x[, 1], ' '))
-dec <- wordrep(dec, c('green', 'greenish'))
-dec <- wordrep(dec, c('red', 'reddish'))
-dec <- wordrep(dec, c('dolomite', 'dolomitic'))
-dec <- wordrep(dec, c('chert', 'cherty'))
-dec <- wordrep(dec, c('sandstone', 'sand', 'sandy'))
-dec <- wordrep(dec, c('shale', 'shaly', 'shaley'))
-dec <- wordrep(dec, c('siltstone', 'silty'))
+
+# replace certain words
+dup.words <- list(c('green', 'greenish'), 
+                  c('red', 'reddish'), 
+                  c('blue', 'bluish'),
+                  c('brown', 'brownish'),
+                  c('yellow', 'yellowish'),
+                  c('thin', 'thinly'),
+                  c('thick', 'thickly'),
+                  c('bedded', 'laminated'),
+                  c('dolomite', 'dolomitic'),
+                  c('chert', 'cherty'),
+                  c('sandstone', 'sand', 'sandy'),
+                  c('mudstone', 'mud'),
+                  c('laminated', 'laminations'),
+                  c('shale', 'shaly', 'shaley'),
+                  c('siltstone', 'silty', 'siliceous'),
+                  c('arkose', 'arkosic'),
+                  c('argillite', 'argillaceous'))
+for(ii in seq(length(dup.words))) {
+  dec <- wordrep(dec, dup.words[[ii]])
+}
+
+#table(Reduce(c, Reduce(c, dec)))
+# cut out specific words
+dec <- wordrm(dec, c('sedimentary', 'light', 'dark', 'white', 
+                     'phosphatic', 'tan', 'yellow', 'medium'))
 dec <- llply(dec, function(y) laply(y, function(x) paste0(x, collapse = ' ')))
 lit <- Map(function(x, y) {x[, 1] <- y; x}, lit, dec)
 
+# get rid of duplicated words because that's stupid
+lit.words <- llply(lit, function(x) str_split(x[, 1], ' '))
+dups <- worddup(lit.words)
+lit <- Map(function(x, y) {x[, 1] <- y; x}, lit, dups)
+
+# short alphabetically
+ss <- llply(lit, function(x) str_split(x[, 1], ' '))
+ss <- llply(ss, function(x) llply(x, sort))
+ss <- llply(ss, function(x) laply(x, function(y) paste0(y, collapse = ' ')))
+lit <- Map(function(x, y) {x[, 1] <- y; x}, lit, ss)
+
+# clear blanks
+ss <- llply(lit, function(x) str_split(x[, 1], ' '))
+ss <- wordrm(ss, c(' '))
+ss <- llply(ss, function(y) laply(y, function(x) paste0(x, collapse = ' ')))
+lit <- Map(function(x, y) {x[, 1] <- y; x}, lit, ss)
+
+# some descriptions have been lost all together
+extrabad <- llply(lit, function(x) x[, 1] != '')
+lit <- Map(function(x, y) x[y, , drop = FALSE], lit, extrabad)
+
 lit <- llply(lit, function(x) aggregate(as.numeric(x[, 2]) ~ x[, 1], FUN = sum))
+lit <- lit[laply(lit, nrow) > 0]
+
 rock.matrix <- lith.matrix(lit)
 rock.matrix <- t(apply(rock.matrix, 1, function(x) x / sum(x)))
 comp.tr <- ilr(rock.matrix)  # isometric log ratio transform
@@ -70,28 +100,31 @@ comp.tr <- ilr(rock.matrix)  # isometric log ratio transform
 #   too many descriptions
 
 # is there a way to programmatically simplify?
+rock.wordfreq <- table(Reduce(c, str_split(colnames(rock.matrix), ' ')))
 too.short <- names(which(rock.wordfreq == 1))
+rock.desc <- Reduce(c, llply(lit, function(x) x[, 1]))
 by.word <- str_split(rock.desc, ' ')
 sho.words <- llply(lit, function(x) str_split(x[, 1], ' '))
-sho.bad <- llply(sho.words, function(y) llply(y, function(x) x %in% too.short))
-sho.words <- Map(function(a, b) Map(function(x, y) x[!y], a, b), 
-                 sho.words, sho.bad)
-sho.words <- llply(sho.words, function(x) 
-                   laply(x, function(y) paste0(y, collapse = ' ')))
-sho <- Map(function(x, y) {x[, 1] <- y; x}, lit, sho.words)
-sho <- llply(sho, function(x) aggregate(as.numeric(x[, 2]) ~ x[, 1], FUN = sum))
+sho <- wordrm(sho.words, too.short)
+sho <- llply(sho, function(y) laply(y, function(x) paste0(x, collapse = ' ')))
+sho <- Map(function(x, y) {x[, 1] <- y; x}, lit, sho)
+sho <- llply(sho , function(x) aggregate(as.numeric(x[, 2]) ~ x[, 1], FUN = sum))
 short.matrix <- lith.matrix(sho)  
 short.matrix <- t(apply(short.matrix, 1, function(x) x / sum(x)))
 shcm.tr <- ilr(short.matrix)  # isometric log ratio transform
-#table(Reduce(c, str_split(unique(Reduce(c, sho.words)), ' ')))
 
 # i still think there are too many lithology types
 # can i do better than this?
 #   colors?
 #   similar words (e.g. shale, shaly)
 still.words <- table(Reduce(c, str_split(colnames(short.matrix), ' ')))
-colnames(short.matrix)
-table(colSums(apply(short.matrix, 2, function(x) x != 0)))
+apcount <- apply(short.matrix, 2, function(x) sum(x > 0))
+solos <- names(which(apcount == 1))
+sw <- apply(short.matrix[, solos], 1, function(x) x > 0)
+
+rock.fossil <- rock.fossil[rownames(rock.fossil) %in% rownames(short.matrix), ]
+
+
 
 
 ## top, bottom age est; continuous time model
