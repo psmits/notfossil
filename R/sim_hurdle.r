cee894959e90a5aadf1cda52b2ade88a017040e0
Peter Smits <thestinkypete@gmail.com>
2017-11-03 11:07:38 -0700

    figure simulating from hurdle model

--- R/sim_hurdle.r
+++ R/sim_hurdle.r
@@ -2,12 +2,13 @@ library(ggplot2)
 library(ggridges)
 library(plyr)
 library(reshape2)
+library(actuar)
 
-rtpois <- function(n, lambda) {
-  # simulate from zero-truncated poisson
-  x <- qpois(runif(n, dpois(0, lambda), 1), lambda)
-  x
-}
+#rtpois <- function(n, lambda) {
+#  # simulate from zero-truncated poisson
+#  x <- qpois(runif(n, dpois(0, lambda), 1), lambda)
+#  x
+#}
 
 rhurdle <- function(n, theta, lambda) {
   # theta: probability of generating a 0
@@ -20,7 +21,8 @@ rhurdle <- function(n, theta, lambda) {
   
   # then lambda
   not.zero <- !is.zero
-  nz <- rtpois(sum(not.zero), lambda)
+  #nz <- rtpois(sum(not.zero), lambda)
+  nz <- rztpois(sum(not.zero), lambda)
 
   # put it together
   out[is.zero == 1] <- 0
@@ -28,26 +30,78 @@ rhurdle <- function(n, theta, lambda) {
 
   out
 }
+roverhurdle <- function(n, theta, size, prob) {
+  # theta: probability of generating a 0
+  # lambda: poisson expectation
+
+  out <- array(dim = n)
   
+  # first theta
+  is.zero <- rbinom(n, 1, theta)
+  
+  # then lambda
+  not.zero <- !is.zero
+  nz <- rztnbinom(sum(not.zero), size, prob)
 
-n <- 5000
+  # put it together
+  out[is.zero == 1] <- 0
+  out[not.zero] <- nz
+
+  out
+}
+
+
+# hurdle with poisson
+
+n <- 1000
 theta <- c(0.1, 0.25, 0.5, 0.75, 0.9)
-lambda <- 5 
+lambda <- c(1, 2, 5, 7, 10)
 
-sim <- alply(theta, 1, function(x) rhurdle(n, x, lambda))
-names(sim) <- theta
-sim <- melt(sim)
+out <- list()
+for(ii in seq(length(lambda))) {
+  out[[ii]] <- alply(theta, 1, function(x) rhurdle(n, x, lambda[ii]))
+  names(out[[ii]]) <- as.character(theta)
+}
+names(out) <- as.character(lambda)
+out <- melt(out)[, -1]
+names(out) <- c('count', 'theta', 'lambda')
+out$theta <- factor(out$theta, levels = theta)
+out$lambda <- factor(out$lambda, levels = lambda)
 
-ds <- data.frame(sim)
-names(ds) <- c('n', 'count', 'sim')
+sim <- ggplot(out, aes(x = count, y = ..density..))
+sim <- sim + geom_histogram()
+sim <- sim + facet_grid(theta ~ lambda)
+sim <- sim + labs(y = 'Density of simultaed counts', x = 'Count')
+sim <- sim + theme_bw()
+sim <- sim + theme(strip.text = element_text(size = 25),
+                   axis.title = element_text(size = 30))
+ggsave(filename = '../doc/figure/hurdle_simulation.png',
+       plot = sim, height = 8, width = 10)
 
 
 
-sim.out <- ggplot(ds, aes(x = count, y = ..count..))
-sim.out <- sim.out + geom_histogram()
-sim.out <- sim.out + facet_grid(sim ~ ., switch = 'y')
-sim.out <- sim.out + labs(y = expression(paste('Simulated value of ', Theta)))
-sim.out <- sim.out + scale_x_continuous(breaks = seq(0, max(ds$count), 1))
-sim.out <- sim.out + theme_minimal()
-sim.out <- sim.out + theme(axis.text.y = element_blank(),
-                           strip.text = element_text(size = 25))
+# hurdle with neg binom
+size <- c(1, 2, 5, 7, 10)
+prob <- theta
+
+out <- list()
+for(ii in seq(length(lambda))) {
+  out[[ii]] <- alply(prob, 1, function(x) 
+                     roverhurdle(n, theta = 0.5, size[ii], x))
+  names(out[[ii]]) <- as.character(prob)
+}
+names(out) <- as.character(lambda)
+out <- melt(out)[, -1]
+names(out) <- c('count', 'prob', 'size')
+out$prob <- factor(out$prob, levels = prob)
+out$size <- factor(out$size, levels = size)
+
+sim2 <- ggplot(out, aes(x = count, y = ..density..))
+sim2 <- sim2 + geom_histogram()
+sim2 <- sim2 + facet_grid(prob ~ size)
+sim2 <- sim2 + labs(y = 'Density of simultaed counts', x = 'Count')
+sim2 <- sim2 + theme_bw()
+sim2 <- sim2 + theme(strip.text = element_text(size = 25),
+                   axis.title = element_text(size = 30))
+ggsave(filename = '../doc/figure/overhurdle_simulation.png',
+       plot = sim2, height = 8, width = 10)
