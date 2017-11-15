f4298376715835b5447701738105224a6e63716d
Peter Smits <thestinkypete@gmail.com>
2017-11-15 15:42:04 -0800

    tons of posterior predictive fits. ilr inv of composition betas

--- R/process_posterior.r
+++ R/process_posterior.r
@@ -10,6 +10,7 @@ library(compositions)
 library(arm)
 library(rstan)
 library(coda)
+library(bayesplot)
 
 source('stan_utility.R')
 source('sim_hurdle.r')
@@ -44,20 +45,27 @@ for(jj in seq(nsim)) {
 # empirical values
 em <- mean(standata$y)
 esd <- sd(standata$y)
-ee <- data.frame(emp = c(em, esd), variable = c('mean', 'sd'))
+emx <- max(standata$y)
+epz <- mean(standata$y == 0)
+ee <- data.frame(emp = c(em, esd, emx, epz), 
+                 variable = c('mean', 'sd', 'max', 'prop'))
 
 # point estimates from sims
 ppc.m <- laply(ppc, mean)
 ppc.sd <- laply(ppc, sd)
+ppc.mx <- laply(ppc, max)
+prop_zero <- function(x) mean(x == 0)
+ppc.prop <- laply(ppc, prop_zero)
 
 # histogram plots
-ppc.sum <- data.frame(mean = ppc.m, sd = ppc.sd)
+ppc.sum <- data.frame(mean = ppc.m, sd = ppc.sd, 
+                      max = ppc.mx, prop = ppc.prop)
 ppc.sum <- melt(ppc.sum)
 
 ppc.gg <- ggplot(ppc.sum, aes(x = value))
 ppc.gg <- ppc.gg + geom_histogram()
 ppc.gg <- ppc.gg + facet_grid(. ~ variable, scales = 'free_x')
-ppc.gg <- ppc.gg + geom_vline(mapping = aes(xintercept = em),
+ppc.gg <- ppc.gg + geom_vline(mapping = aes(xintercept = emp),
                               data = ee,
                               size = 1.5, colour = 'blue')
 
@@ -73,6 +81,20 @@ ppc.res <- melt(ppc.res)
 res.gg <- ggplot(ppc.res, aes(x = value))
 res.gg <- res.gg + geom_histogram()
 res.gg <- res.gg + facet_grid(. ~ variable, scales = 'free_x')
+
+es <- ppc_error_scatter(standata$y, Reduce(rbind, ppc[1:6]))
+esa <- ppc_error_scatter_avg(standata$y, Reduce(rbind, ppc))
+estecdf <- ppc_ecdf_overlay(standata$y, Reduce(rbind, ppc[1:50]))
+
+# rootograms because i find them easy to read
+rot.gg <- ppc_rootogram(standata$y, 
+                        Reduce(rbind, ppc), 
+                        style = 'hanging',
+                        prob = 0.8)
+#### enough posterior predictive analyses
+
+
+
 
 
 # theta regression coefficients
@@ -101,10 +123,19 @@ lam.gg <- lam.gg + labs(x = 'estimate', y = 'predictor of lambda')
 
 
 # back transform the compositional variables
-tt <- colMeans(post$beta_the[, 1:22])
-ll <- colMeans(post$beta_lam[, 1:22])
-dim(post$beta_the[, 1:22])
-inv.theta <- alply(post$beta_the[, 1:22], 2, function(x) 
-                   ilrInv(x, orig = unit.info$lithology$raw))
-inv.lambda <- alply(post$beta_lam[, 1:22], 2, function(x) 
-                    ilrInv(x, orig = unit.info$lithology$raw))
+inv.the <- alply(post$beta_the[, 1:22], 1, function(x) ilrInv(x, orig = unit.info$lithology$raw))
+the.comp.max <- table(laply(inv.the, which.max))
+inv.the.m <- melt(Reduce(cbind, inv.the))[, c(1, 3)]
+inv.the.m$Var1 <- factor(inv.the.m$Var1)
+inv.the.gg <- ggplot(inv.the.m, aes(x = value, y = Var1))
+inv.the.gg <- inv.the.gg + geom_density_ridges(rel_min_height = 0.01)
+inv.the.gg <- inv.the.gg + theme_ridges()
+
+
+inv.lam <- alply(post$beta_lam[, 1:22], 1, function(x) ilrInv(x, orig = unit.info$lithology$raw))
+lam.comp.max <- table(laply(inv.lam, which.max))
+inv.lam.m <- melt(Reduce(cbind, inv.lam))[, c(1, 3)]
+inv.lam.m$Var1 <- factor(inv.lam.m$Var1)
+inv.lam.gg <- ggplot(inv.lam.m, aes(x = value, y = Var1))
+inv.lam.gg <- inv.lam.gg + geom_density_ridges(rel_min_height = 0.01)
+inv.lam.gg <- inv.lam.gg + theme_ridges()
