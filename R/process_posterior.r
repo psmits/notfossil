1204d3a19b2bb468e8da603ad3c9b907636eb374
Peter Smits <thestinkypete@gmail.com>
2017-11-16 12:08:00 -0800

    neg bin sims working. wrappers on posterior process

--- R/process_posterior.r
+++ R/process_posterior.r
@@ -1,24 +1,29 @@
+# manipulation
 library(reshape2)
 library(plyr)
 
-library(ggplot2)
-library(scales)
-library(grid)
-library(ggridges)
-
+# analysis
 library(compositions)
 library(arm)
 library(rstan)
 library(coda)
+
+# plotting
+library(ggplot2)
+library(scales)
+library(grid)
+library(ggridges)
 library(bayesplot)
 
+# helpful functions
 source('stan_utility.R')
 source('sim_hurdle.r')
 
+# set up data
+load(file = '../data/data_dump/unit_image.rdata')
+
 nsim <- 1000
 grab <- sample(standata$N, nsim)
-
-load(file = '../data/data_dump/unit_image.rdata')
 
 files <- list.files('../data/mcmc_out', pattern = 'hurdle', full.names = TRUE)
 
@@ -32,10 +37,11 @@ post <- rstan::extract(fit, permuted = TRUE)
 fit2 <- read_stan_csv(files[5:8])
 check_all_diagnostics(fit2)
 check_treedepth(fit2, max_depth = 15)
-post2 <- rstan::extract(fit, permuted = TRUE)
+post2 <- rstan::extract(fit2, permuted = TRUE)
 
 # posterior predictive simulations
-ppc <- list()
+#ppc.sim.check <- function(post) {}
+ppc.p <- list()
 for(jj in seq(nsim)) {
   gg <- grab[jj]
   oo <- c()
@@ -44,73 +50,109 @@ for(jj in seq(nsim)) {
                       theta = post$theta[gg, ii], 
                       lambda = post$lambda_est[gg, ii])
   }
-  ppc[[jj]] <- oo
+  ppc.p[[jj]] <- oo
+}
+ppc.nb <- list()
+for(jj in seq(nsim)) {
+  gg <- grab[jj]
+  oo <- c()
+  for(ii in seq(standata$N)) {
+    oo[ii] <- roverhurdle(1, 
+                          theta = post2$theta[gg, ii], 
+                          mu = post2$lambda_est[gg, ii],
+                          omega = post2$phi[gg])
+  }
+  ppc.nb[[jj]] <- oo
 }
 
+series.checks <- function(standata, ppc) {
+  # point estimates
+  ppc <- Reduce(rbind, ppc)
+  ppc.mean <- ppc_stat(standata$y, ppc, stat = 'mean')
+  ppc.sd <- ppc_stat(standata$y, ppc, stat = 'sd')
+  ppc.max <- ppc_stat(standata$y, ppc, stat = 'max')
+  prop_zero <- function(x) mean(x == 0)
+  ppc.zero <- ppc_stat(standata$y, ppc, stat = 'prop_zero')
+  q25 <- function(x) quantile(x, 0.25)
+  ppc.q25 <- ppc_stat(standata$y, ppc, stat = 'q25')
+  q75 <- function(x) quantile(x, 0.75)
+  ppc.q75 <- ppc_stat(standata$y, ppc, stat = 'q75')
+
+  # error
+  ppc.err <- ppc_error_scatter(standata$y, ppc[1:6, ])
+  ppc.avgerr <- ppc_error_scatter_avg(standata$y, ppc)
+  ppc.ecdf <- ppc_ecdf_overlay(standata$y, ppc[1:50, ])
+
+  # rootograms because i find them easy to read
+  root.gg <- ppc_rootogram(standata$y, ppc, style = 'hanging', prob = 0.8)
+
+  # all the plots
+  out <- list(mean = ppc.mean, 
+              sd = ppc.sd, 
+              max = ppc.max, 
+              zero = ppc.zero, 
+              q25 = ppc.q25,
+              q75 = ppc.q75,
+              err = ppc.err, 
+              avgerr = ppc.avgerr, 
+              ecdf = ppc.ecdf, 
+              root = root.gg)
+  out
+}
+po.check <- series.checks(standata, ppc.p)
+nb.check <- series.checks(standata, ppc.nb)
 
 
-# point estimates
-ppc.mean <- ppc_stat(standata$y, Reduce(rbind, ppc), stat = 'mean')
-ppc.sd <- ppc_stat(standata$y, Reduce(rbind, ppc), stat = 'sd')
-ppc.max <- ppc_stat(standata$y, Reduce(rbind, ppc), stat = 'max')
-prop_zero <- function(x) mean(x == 0)
-ppc.zero <- ppc_stat(standata$y, Reduce(rbind, ppc), stat = 'prop_zero')
 
-# error
-ppc.err <- ppc_error_scatter(standata$y, Reduce(rbind, ppc[1:6]))
-ppc.avgerr <- ppc_error_scatter_avg(standata$y, Reduce(rbind, ppc))
-ppc.ecdf <- ppc_ecdf_overlay(standata$y, Reduce(rbind, ppc[1:50]))
+post.vis <- function(post, unit.info) {
+  # theta regression coefficients
+  bet.the <- post$beta_the
+  colnames(bet.the) <- c(colnames(unit.info$lithology$raw)[-1], 
+                         'thickness', 'area', 'contact above', 
+                         'contact below', 'subsurface')
+  bet.the <- melt(bet.the)
 
-# rootograms because i find them easy to read
-rot.gg <- ppc_rootogram(standata$y, 
-                        Reduce(rbind, ppc), 
-                        style = 'hanging',
-                        prob = 0.8)
-#### enough posterior predictive analyses
+  the.gg <- ggplot(bet.the, aes(x = value, y = Var2))
+  the.gg <- the.gg + geom_density_ridges(rel_min_height = 0.01)
+  the.gg <- the.gg + theme_ridges()
+  the.gg <- the.gg + labs(x = 'estimate', y = 'predictor of theta')
 
+  # lambda regression coefficients
+  bet.lam <- post$beta_lam
+  colnames(bet.lam) <- c(colnames(unit.info$lithology$raw)[-1], 
+                         'thickness', 'area', 'contact above', 
+                         'contact below', 'subsurface')
+  bet.lam <- melt(bet.lam)
 
-
-
-
-# theta regression coefficients
-bet.the <- post$beta_the
-colnames(bet.the) <- c(colnames(unit.info$lithology$raw)[-1], 
-                       'thickness', 'area', 'contact above', 
-                       'contact below', 'subsurface')
-bet.the <- melt(bet.the)
-
-the.gg <- ggplot(bet.the, aes(x = value, y = Var2))
-the.gg <- the.gg + geom_density_ridges(rel_min_height = 0.01)
-the.gg <- the.gg + theme_ridges()
-the.gg <- the.gg + labs(x = 'estimate', y = 'predictor of theta')
-
-# lambda regression coefficients
-bet.lam <- post$beta_lam
-colnames(bet.lam) <- c(colnames(unit.info$lithology$raw)[-1], 
-                       'thickness', 'area', 'contact above', 
-                       'contact below', 'subsurface')
-bet.lam <- melt(bet.lam)
-
-lam.gg <- ggplot(bet.lam, aes(x = value, y = Var2))
-lam.gg <- lam.gg + geom_density_ridges(rel_min_height = 0.01)
-lam.gg <- lam.gg + theme_ridges()
-lam.gg <- lam.gg + labs(x = 'estimate', y = 'predictor of lambda')
+  lam.gg <- ggplot(bet.lam, aes(x = value, y = Var2))
+  lam.gg <- lam.gg + geom_density_ridges(rel_min_height = 0.01)
+  lam.gg <- lam.gg + theme_ridges()
+  lam.gg <- lam.gg + labs(x = 'estimate', y = 'predictor of lambda')
 
 
-# back transform the compositional variables
-inv.the <- alply(post$beta_the[, 1:22], 1, function(x) ilrInv(x, orig = unit.info$lithology$raw))
-the.comp.max <- table(laply(inv.the, which.max))
-inv.the.m <- melt(Reduce(cbind, inv.the))[, c(1, 3)]
-inv.the.m$Var1 <- factor(inv.the.m$Var1)
-inv.the.gg <- ggplot(inv.the.m, aes(x = value, y = Var1))
-inv.the.gg <- inv.the.gg + geom_density_ridges(rel_min_height = 0.01)
-inv.the.gg <- inv.the.gg + theme_ridges()
+  # back transform the compositional variables
+  inv.the <- alply(post$beta_the[, 1:22], 1, function(x) ilrInv(x, orig = unit.info$lithology$raw))
+  the.comp.max <- table(laply(inv.the, which.max))
+  inv.the.m <- melt(Reduce(cbind, inv.the))[, c(1, 3)]
+  inv.the.m$Var1 <- factor(inv.the.m$Var1)
+  inv.the.gg <- ggplot(inv.the.m, aes(x = value, y = Var1))
+  inv.the.gg <- inv.the.gg + geom_density_ridges(rel_min_height = 0.01)
+  inv.the.gg <- inv.the.gg + theme_ridges()
 
 
-inv.lam <- alply(post$beta_lam[, 1:22], 1, function(x) ilrInv(x, orig = unit.info$lithology$raw))
-lam.comp.max <- table(laply(inv.lam, which.max))
-inv.lam.m <- melt(Reduce(cbind, inv.lam))[, c(1, 3)]
-inv.lam.m$Var1 <- factor(inv.lam.m$Var1)
-inv.lam.gg <- ggplot(inv.lam.m, aes(x = value, y = Var1))
-inv.lam.gg <- inv.lam.gg + geom_density_ridges(rel_min_height = 0.01)
-inv.lam.gg <- inv.lam.gg + theme_ridges()
+  inv.lam <- alply(post$beta_lam[, 1:22], 1, function(x) ilrInv(x, orig = unit.info$lithology$raw))
+  lam.comp.max <- table(laply(inv.lam, which.max))
+  inv.lam.m <- melt(Reduce(cbind, inv.lam))[, c(1, 3)]
+  inv.lam.m$Var1 <- factor(inv.lam.m$Var1)
+  inv.lam.gg <- ggplot(inv.lam.m, aes(x = value, y = Var1))
+  inv.lam.gg <- inv.lam.gg + geom_density_ridges(rel_min_height = 0.01)
+  inv.lam.gg <- inv.lam.gg + theme_ridges()
+
+  out <- list(theta = the.gg,
+              inv.theta = inv.the.gg,
+              lambda = lam.gg,
+              inv.lambda = inv.lam.gg)
+  out
+}
+po.vis <- post.vis(post, unit.info)
+nb.vis <- post.vis(post2, unit.info)
--- R/sim_hurdle.r
+++ R/sim_hurdle.r
@@ -2,7 +2,8 @@ library(ggplot2)
 library(ggridges)
 library(plyr)
 library(reshape2)
-library(actuar)
+#install.packages("countreg", repos="http://R-Forge.R-project.org")
+library(countreg)
 
 #rtpois <- function(n, lambda) {
 #  # simulate from zero-truncated poisson
@@ -21,7 +22,6 @@ rhurdle <- function(n, theta, lambda) {
 
   # then lambda
   not.zero <- !is.zero
-  #nz <- rtpois(sum(not.zero), lambda)
   nz <- rztpois(sum(not.zero), lambda)
 
   # put it together
@@ -30,7 +30,7 @@ rhurdle <- function(n, theta, lambda) {
 
   out
 }
-roverhurdle <- function(n, theta, mean, overdispersion) {
+roverhurdle <- function(n, theta, mu, omega) {
   # theta: probability of generating a 0
   # lambda: poisson expectation
 
@@ -41,11 +41,7 @@ roverhurdle <- function(n, theta, mean, overdispersion) {
 
   # then lambda
   not.zero <- !is.zero
-  # a is # fails
-  # p is prob
-  # mean = a / b
-  # overdisp = 1 + 1 / b
-  nz <- rztnbinom(sum(not.zero), size, prob)
+  nz <- rztnbinom(sum(not.zero), mu = mu, theta = omega)
 
   # put it together
   out[is.zero == 1] <- 0
@@ -55,60 +51,60 @@ roverhurdle <- function(n, theta, mean, overdispersion) {
 }
 
 
-# hurdle with poisson
-
-n <- 1000
-theta <- c(0.1, 0.25, 0.5, 0.75, 0.9)
-lambda <- c(1, 2, 5, 7, 10)
-
-out <- list()
-for(ii in seq(length(lambda))) {
-  out[[ii]] <- alply(theta, 1, function(x) rhurdle(n, x, lambda[ii]))
-  names(out[[ii]]) <- as.character(theta)
-}
-names(out) <- as.character(lambda)
-out <- melt(out)[, -1]
-names(out) <- c('count', 'theta', 'lambda')
-out$theta <- factor(out$theta, levels = theta)
-out$lambda <- factor(out$lambda, levels = lambda)
-
-sim <- ggplot(out, aes(x = count, y = ..density..))
-sim <- sim + geom_histogram()
-sim <- sim + facet_grid(theta ~ lambda)
-sim <- sim + labs(y = 'Density of simultaed counts', x = 'Count')
-sim <- sim + theme_bw()
-sim <- sim + theme(strip.text = element_text(size = 25),
-                   axis.title = element_text(size = 30),
-                   axis.text = element_text(size = 20))
-ggsave(filename = '../doc/figure/hurdle_simulation.png',
-       plot = sim, height = 8, width = 10)
-
-
-
-# hurdle with neg binom
-size <- c(1, 2, 5, 7, 10)
-prob <- theta
-
-out <- list()
-for(ii in seq(length(lambda))) {
-  out[[ii]] <- alply(prob, 1, function(x) 
-                     roverhurdle(n, theta = 0.5, size[ii], x))
-  names(out[[ii]]) <- as.character(prob)
-}
-names(out) <- as.character(lambda)
-out <- melt(out)[, -1]
-names(out) <- c('count', 'prob', 'size')
-out$prob <- factor(out$prob, levels = prob)
-out$size <- factor(out$size, levels = size)
-
-sim2 <- ggplot(out, aes(x = count, y = ..density..))
-sim2 <- sim2 + geom_histogram()
-sim2 <- sim2 + facet_grid(prob ~ size)
-sim2 <- sim2 + labs(y = 'Density of simultaed counts', x = 'Count')
-sim2 <- sim2 + theme_bw()
-sim2 <- sim2 + theme(strip.text = element_text(size = 25),
-                     axis.title = element_text(size = 30),
-                     axis.text = element_text(size = 20))
-ggsave(filename = '../doc/figure/overhurdle_simulation.png',
-       plot = sim2, height = 8, width = 10)
-
+## hurdle with poisson
+#
+#n <- 1000
+#theta <- c(0.1, 0.25, 0.5, 0.75, 0.9)
+#lambda <- c(1, 2, 5, 7, 10)
+#
+#out <- list()
+#for(ii in seq(length(lambda))) {
+#  out[[ii]] <- alply(theta, 1, function(x) rhurdle(n, x, lambda[ii]))
+#  names(out[[ii]]) <- as.character(theta)
+#}
+#names(out) <- as.character(lambda)
+#out <- melt(out)[, -1]
+#names(out) <- c('count', 'theta', 'lambda')
+#out$theta <- factor(out$theta, levels = theta)
+#out$lambda <- factor(out$lambda, levels = lambda)
+#
+#sim <- ggplot(out, aes(x = count, y = ..density..))
+#sim <- sim + geom_histogram()
+#sim <- sim + facet_grid(theta ~ lambda)
+#sim <- sim + labs(y = 'Density of simultaed counts', x = 'Count')
+#sim <- sim + theme_bw()
+#sim <- sim + theme(strip.text = element_text(size = 25),
+#                   axis.title = element_text(size = 30),
+#                   axis.text = element_text(size = 20))
+#ggsave(filename = '../doc/figure/hurdle_simulation.png',
+#       plot = sim, height = 8, width = 10)
+#
+#
+#
+## hurdle with neg binom
+#size <- c(1, 2, 5, 7, 10)
+#prob <- theta
+#
+#out <- list()
+#for(ii in seq(length(lambda))) {
+#  out[[ii]] <- alply(prob, 1, function(x) 
+#                     roverhurdle(n, theta = 0.5, size[ii], x))
+#  names(out[[ii]]) <- as.character(prob)
+#}
+#names(out) <- as.character(lambda)
+#out <- melt(out)[, -1]
+#names(out) <- c('count', 'prob', 'size')
+#out$prob <- factor(out$prob, levels = prob)
+#out$size <- factor(out$size, levels = size)
+#
+#sim2 <- ggplot(out, aes(x = count, y = ..density..))
+#sim2 <- sim2 + geom_histogram()
+#sim2 <- sim2 + facet_grid(prob ~ size)
+#sim2 <- sim2 + labs(y = 'Density of simultaed counts', x = 'Count')
+#sim2 <- sim2 + theme_bw()
+#sim2 <- sim2 + theme(strip.text = element_text(size = 25),
+#                     axis.title = element_text(size = 30),
+#                     axis.text = element_text(size = 20))
+#ggsave(filename = '../doc/figure/overhurdle_simulation.png',
+#       plot = sim2, height = 8, width = 10)
+#
