af6b7031c41ade9116ee3f4648c70821d0a352a6
psmits <thestinkypete@gmail.com>
2018-01-22 17:41:26 -0800

    making unit plots. need to plot collection times

--- R/data_plots.r
+++ R/data_plots.r
@@ -1,36 +1,108 @@
 library(reshape2)
 library(plyr)
+library(stringr)
 
 library(ggplot2)
 library(scales)
 library(grid)
 library(ggridges)
+theme_set(theme_bw())  # graph setting
 
-library(arm)
-library(rstan)
-library(coda)
+source('../R/rock_functions.r')
+source('../R/rock_mung.r')
+source('../R/fossil_functions.r')
+source('../R/fossil_mung.r')
 
-library(brms)
-library(pscl)
+source('../R/download_scrap.r')  # just macrostrat
 
-
-load(file = '../data/data_dump/unit_image.rdata')
-
-
-ord.units <- data.frame(x = standata$y)
-
-op <- ggplot(ord.units, aes(x = x, y = ..density..))
-op <- op + geom_histogram()
-op <- op + labs(y = 'Density of counts', x = 'Count')
-op <- op + theme_bw()
-op <- op + theme(axis.title = element_text(size = 30),
-                 axis.text = element_text(size = 20))
-ggsave(filename = '../doc/figure/ordovician_counts.png',
-       plot = op, height = 8, width = 10)
+# constants
+shelly <- c('Brachiopoda', 'Trilobita', 'Bivalvia', 'Gastropoda')
+ord <- c(460.4, 443.8)
+mid <- 445.6
+bracket <- c(ord[1], mid, ord[2])
 
 
-dfs <- data.frame(y = standata$y, standata$X)
-brm(y ~ X1 + X2 + X3 + X4 + X5 + X6 + X7 + X8 + X9 + X10
-    + X11 + X12 + X13 + X14 + X15 + X16 + X17 + X18 + X19
-    + X20 + X21 + X22 + X23 + X24 + X25 + X26
-    , data = dfs, family = hurdle_poisson())
+# have yes/no label based on taxonomy
+unit.dur <- data.frame(id = strat.ord$unit_id,
+                       start = strat.ord$b_age,
+                       stop = strat.ord$t_age)
+for(ii in seq(length(shelly))) {
+  foss.info <- process.fossil(fossil.ord, shelly = shelly[ii])
+  yesno <- (as.character(strat.ord$unit_id) %in% foss.info$unit) * 1
+  unit.dur[, ncol(unit.dur) + 1] <- yesno
+}
+names(unit.dur) <- shelly
+unit.dur <- unit.dur[order(unit.dur$start), ] 
+unit.dur$id <- factor(unit.dur$id, levels = unique(unit.dur$id))
+
+# collection dates
+collecs <- data.frame(id = collec.ord$collection_no,
+                      age = apply(collec.ord[, c('max_ma', 'min_ma')], 1, mean))
+
+# next goal is put collection X-s in
+unit.dur$id %in% factor(unique(foss.info$unit))
+
+
+
+
+oo <- list()
+for(ii in seq(length(shelly))) {
+  tt <- which(names(unit.dur) == shelly[ii])
+  oo[[ii]] <- unit.dur[, c(1:3, tt)]
+  names(oo[[ii]]) <- c(names(unit.dur)[1:3], 'fossils')
+  oo[[ii]]$taxon <- shelly[ii]
+}
+unit.dur <- Reduce(rbind, oo)
+unit.dur$fossils <- mapvalues(unit.dur$fossils,
+                              from = 0:1,
+                              to = c('no', 'yes'))
+
+
+
+
+
+# skeleton graph
+unitgg <- ggplot(unit.dur, aes(x = start, xend = stop, 
+                                  y = id, yend = id, 
+                                  colour = fossils))
+unitgg <- unitgg + geom_segment()
+unitgg <- unitgg + geom_vline(xintercept = 445.6)
+unitgg <- unitgg + facet_wrap( ~ taxon)
+unitgg <- unitgg + coord_cartesian(xlim = c(460.4, 443.8))
+unitgg <- unitgg + scale_colour_manual(values = c('skyblue', 'goldenrod'),
+                                       name = 'Fossil bearing?')
+unitgg <- unitgg + labs(x = 'Time (Mya)', y = 'Macrostrat unit')
+unitgg <- unitgg + theme(axis.text.y = element_blank(),
+                         axis.ticks.y = element_blank(),
+                         legend.position = 'top')
+
+
+
+# restricted data set
+#   start/stop in late ordovician
+#   start/stop before hirnantian
+#   start/stop during hirnantian
+unit.info <- process.strat(strat.ord, bracket = bracket)  
+yesno <- (as.character(unit.info$unit.id) %in% foss.info$unit) * 1
+
+unit.dur <- data.frame(id = unit.info$unit.id,
+                          fossils = mapvalues(yesno,
+                                              from = 0:1,
+                                              to = c('no', 'yes')),
+                          start = unit.info$age$bottom.age,
+                          stop = unit.info$age$top.age)
+unit.dur <- unit.dur[order(unit.dur$start), ] 
+unit.dur$id <- factor(unit.dur$id, levels = unique(unit.dur$id))
+
+restgg <- ggplot(unit.dur, aes(x = start, xend = stop, 
+                                  y = id, yend = id, 
+                                  colour = fossils))
+restgg <- restgg + geom_segment()
+restgg <- restgg + geom_vline(xintercept = 445.6)
+restgg <- restgg + coord_cartesian(xlim = c(460.4, 443.8))
+restgg <- restgg + scale_colour_manual(values = c('skyblue', 'goldenrod'),
+                                       name = 'Fossil bearing?')
+restgg <- restgg + labs(x = 'Time (Mya)', y = 'Macrostrat unit')
+restgg <- restgg + theme(axis.text.y = element_blank(),
+                         axis.ticks.y = element_blank(),
+                         legend.position = 'top')
--- R/download_scrap.r
+++ R/download_scrap.r
@@ -13,3 +13,13 @@ fossil.url <- paste0('https://macrostrat.org/api/v2/fossils?unit_id=',
                      paste0(strat.sil$unit_id, collapse = ','), 
                      '&response=long&format=csv') 
 fossil.sil <- read.csv(fossil.url, stringsAsFactors = FALSE)
+
+# collection information
+fossil.url <- paste0('https://paleobiodb.org/data1.2/colls/list.txt?coll_id=',
+                     paste0(fossil.ord$cltn_id, collapse = ','), 
+                     '&show=full')
+collec.ord <- read.csv(fossil.url, stringsAsFactors = FALSE)
+fossil.url <- paste0('https://paleobiodb.org/data1.2/colls/list.txt?coll_id=',
+                     paste0(fossil.sil$cltn_id, collapse = ','), 
+                     '&show=full')
+collec.sil <- read.csv(fossil.url, stringsAsFactors = FALSE)
