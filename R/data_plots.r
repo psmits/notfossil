c7f9d15298d3c980c6f00e634000299940655915
psmits <thestinkypete@gmail.com>
2018-01-25 15:41:38 -0800

    functional data plot/visual

--- R/data_plots.r
+++ R/data_plots.r
@@ -7,6 +7,8 @@ library(scales)
 library(grid)
 library(ggridges)
 theme_set(theme_bw())  # graph setting
+blind <- c("#999999", "#E69F00", "#56B4E9", "#009E73", 
+           "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
 source('../R/rock_functions.r')
 source('../R/rock_mung.r')
@@ -21,125 +23,79 @@ shelly <- shelly[1]
 ord <- c(460.4, 443.8)
 mid <- 445.6
 
+# macrostrat strat data from the orodovician
+strat.ord <- read.csv('https://macrostrat.org/api/v2/units?interval_name=Ordovician&response=long&format=csv', 
+                      stringsAsFactors = FALSE)
+strat.ord$m_age <- apply(strat.ord[, c('t_age', 'b_age')], 1, mean)
 
-# by genus id
-genus.ids <- laply(fossil.ord$genus_no, function(x) str_split(x, '\\|'))
-names(genus.ids) <- fossil.ord$unit_id
-unique.taxa <- unique(unlist(genus.ids))
-genusy <- melt(genus.ids)  # genus id, unit id
-names(genusy) <- c('taxon_id', 'unit_id')
+# macrostrat fossil data from the orodovician
+fossil.url <- paste0('https://macrostrat.org/api/v2/fossils?unit_id=', 
+                     paste0(strat.ord$unit_id, collapse = ','), 
+                     '&response=long&format=csv') 
+fossil.ord <- read.csv(fossil.url, stringsAsFactors = FALSE)
+# cypher for which collections in what units
+cltn2unit <- fossil.ord[, c('cltn_id', 'unit_id')]
 
-# grab genus information from pbdb
-fossil.url <- paste0('https://paleobiodb.org/data1.2/occs/list.txt?taxon_id=',
-                     paste0(unique.taxa, collapse = ','), 
+
+
+# grab genus information from pbdb based on what occurres in collections
+fossil.url <- paste0('https://paleobiodb.org/data1.2/occs/list.txt?coll_id=',
+                     paste0(fossil.ord$cltn_id, collapse = ','), 
                      '&min_ma=443.8&max_ma=460.4&show=full')
 taxon <- read.csv(fossil.url, stringsAsFactors = FALSE)
-# how many taxon ids returned entries
-taxon.percent <- sum(unique.taxa %in% taxon$accepted_no) / length(unique.taxa)
 
+# associate each fossil occurrence with a macrostrat unit
+taxon$unit <- cltn2unit[match(taxon$collection_no, cltn2unit[, 1]), 2]
+taxon <- taxon[taxon$phylum %in% shelly, ]  # just the taxa of interest
 
+# put occurrences at middle of unit
+ss <- fossil.ord[, c('unit_id', 'b_age', 't_age')]
+ss$mid <- apply(ss[, 2:3], 1, mean)
+taxon$mid_ma <- ss$mid[match(taxon$unit, ss$unit_id)]
 
-# do collections make sense temporally?
-# lets look just in macrostrat
-times <- strat.ord[, c('unit_id', 't_age', 'b_age')]
-ff <- fossil.ord[, c('cltn_id', 'unit_id', 't_age', 'b_age')]
-ffunit <- times[times$unit_id %in% ff$unit_id, ]
+# make nice
+strat.ord <- strat.ord[order(strat.ord$b_age), ]
+strat.ord$unit_id <- factor(strat.ord$unit_id, levels = unique(strat.ord$unit_id))
+taxon$unit <- factor(taxon$unit, levels = levels(strat.ord$unit_id))
 
-exu <- ffunit[ffunit$unit_id == ff$unit_id[1], ]
-ex <- ff[ff$unit_id == ff$unit_id[1], ]
+strat.ord$fossils <- factor((strat.ord$unit_id %in% taxon$unit))
 
-# macrostrat collection age is identical to macrostrat unit age
-#   no superposition information recorded here
-macro.compare <- all(apply(ex[, c('t_age', 'b_age')], 1, function(x) 
-                           all(x == exu[, c('t_age', 'b_age')])))
+tt <- split(taxon, as.character(taxon$unit))
+tt <- llply(tt, function(x) x[!duplicated(x$genus), ])
+taxon <- Reduce(rbind, tt)
+unit.diversity <- laply(tt, nrow)
 
-
-# now check if pbdb collection age is at least similar to macrostrat age
-# grab collection information from pbdb
-fossil.url <- paste0('https://paleobiodb.org/data1.2/colls/list.txt?coll_id=',
-                     paste0(fossil.ord$cltn_id, collapse = ','), 
-                     '&show=full')
-cltn <- read.csv(fossil.url, stringsAsFactors = FALSE)
-cltn.percent <- sum(fossil.ord$cltn_id %in% cltn$collection_no) /
-  length(fossil.ord$cltn_id)
-
-cc <- cltn[, c('collection_no', 'min_ma', 'max_ma')]
-ffcc <- ff[ff$cltn_id %in% cc$collection_no, ]
-
-exc <- ffcc[ffcc$cltn_id == cc$collection_no[1], ]  # based on macrostat
-ex <- cc[cc$collection_no == cc$collection_no[1], ]  # based on pbdb
-
-  
-  
-# what specific taxonomic group(s)
-taxon <- taxon[taxon$phylum %in% shelly | taxon$class %in% shelly, ]  # just good ones
-
-# chosen fossils in units
-melty <- melty[melty$taxon_id %in% taxon$taxon_no, ]
-
-
-# stratigraphic units
-unit.dur <- data.frame(id = strat.ord$unit_id,
-                       start = strat.ord$b_age,
-                       stop = strat.ord$t_age)
-# have fossil of interest
-unit.dur$yesno <- factor((unit.dur$id %in% melty$unit_id) * 1)  
-unit.dur <- unit.dur[order(unit.dur$start), ] 
-unit.dur$id <- factor(unit.dur$id, levels = unique(unit.dur$id))
-
-
-# get age of fossil occurrences
-# use the age data from macrostrat
-uni.collec <- unique(fossil.ord$cltn_id)
-fossil.url <- paste0('https://paleobiodb.org/data1.2/colls/list.txt?coll_id=',
-                     paste0(uni.collec, collapse = ','), 
-                     '&show=full')
-collec <- read.csv(fossil.url, stringsAsFactors = FALSE)
-# collections have ages
-collec <- collec[, c('collection_no', 'max_ma', 'min_ma')]
-# calculate collection age
-collec$mid_ma <- apply(collec[, c('max_ma', 'min_ma')], 1, mean)
-
-
-# stick that back into fossil.ord
-fossil.ord$mid_ma <- NA
-for(ii in seq(nrow(fossil.ord))) {
-  mm <- collec[collec$collection_no %in% fossil.ord$cltn_id[ii], ]
-  if(nrow(mm) > 0) fossil.ord$mid_ma[ii] <- mm$mid_ma
-}
-fossil.ord$unit_id <- factor(fossil.ord$unit_id, levels = levels(unit.dur$id))
-
-
-
-
-
-
-# PBDB dates don't line up with macrostrat dates
-
-
-
-
-
-
-# now i have two data frames
-#   unit.dur: geologic units, start stop times, if they bear fossils
-#   fosocc: fossil occurrence times by taxonomy
-#     note, currently only works for single taxonomic group (e.g. brachiopods)
-
-# skeleton graph
-unitgg <- ggplot(unit.dur, aes(x = start, y = id, 
-                               colour = yesno))
-unitgg <- unitgg + geom_segment(aes(xend = stop, yend = id))
-unitgg <- unitgg + geom_point(data = fossil.ord, 
+# the plot
+unitgg <- ggplot(strat.ord, aes(x = b_age, y = unit_id, colour = fossils)) 
+unitgg <- unitgg + geom_segment(mapping = aes(xend = t_age, yend = unit_id),
+                                alpha = 0.5)
+unitgg <- unitgg + geom_point(mapping = aes(x = m_age))
+unitgg <- unitgg + geom_point(data = taxon,
                               mapping = aes(x = mid_ma, 
-                                            y = unit_id, 
-                                            colour = NULL),
-                              alpha = 0.1)
-unitgg <- unitgg + geom_vline(xintercept = mid)
-unitgg <- unitgg + coord_cartesian(xlim = ord)
-unitgg <- unitgg + scale_colour_manual(values = c('skyblue', 'goldenrod'),
-                                       name = 'Fossil bearing?')
-unitgg <- unitgg + labs(x = 'Time (Mya)', y = 'Macrostrat unit')
+                                            y = unit, 
+                                            colour = NULL), 
+                              shape = 4, alpha = 0.1, size = 1.1)
+unitgg <- unitgg + labs(x = 'Mya', y = 'macrostrat unit')
+unitgg <- unitgg + geom_vline(xintercept = mid, 
+                              linetype = 'dashed',
+                              colour = 'blue')  # start of hirnantian
+unitgg <- unitgg + coord_cartesian(xlim = ord)  # zoom in on ordovician
 unitgg <- unitgg + theme(axis.text.y = element_blank(),
-                         axis.ticks.y = element_blank(),
-                         legend.position = 'top')
+                         axis.ticks.y = element_blank())
+unitgg <- unitgg + scale_colour_manual(values = blind)
+
+# now figure out logical breaks
+timerange <- abs(diff(ord))
+brks <- timerange / 10
+brks <- data.frame(x = seq(from = ord[2], to = ord[1], by = brks))
+unitgg <- unitgg + geom_vline(data = brks, 
+                              mapping = aes(xintercept = x), 
+                              linetype = 'dotted', 
+                              colour = 'darkred')
+# mark beginning, end ordovician
+odf <- data.frame(x = ord)
+unitgg <- unitgg + geom_vline(data = odf, 
+                              mapping = aes(xintercept = x),
+                              colour = 'black', size = 1.5)
+ggsave(plot = unitgg, filename = '../doc/figure/data_plot_brach.png',
+       height = 5.5, width = 8)
--- R/issue_reproduce.r
+++ R/issue_reproduce.r
@@ -23,56 +23,8 @@ fossil.url <- paste0('https://macrostrat.org/api/v2/fossils?unit_id=',
                      '&response=long&format=csv') 
 ord <- read.csv(fossil.url, stringsAsFactors = FALSE)
 
-# by genus id
-genus.ids <- laply(ord$genus_no, function(x) str_split(x, '\\|'))
-unique.taxa <- unique(unlist(genus.ids))
-
-# grab genus information from pbdb
-fossil.url <- paste0('https://paleobiodb.org/data1.2/occs/list.txt?taxon_id=',
-                     paste0(unique.taxa, collapse = ','), 
+# grab genus information from pbdb based on what occurres in collections
+fossil.url <- paste0('https://paleobiodb.org/data1.2/occs/list.txt?coll_id=',
+                     paste0(ord$cltn_id, collapse = ','), 
                      '&min_ma=443.8&max_ma=460.4&show=full')
 taxon <- read.csv(fossil.url, stringsAsFactors = FALSE)
-# how many taxon ids returned entries
-taxon.percent <- sum(unique.taxa %in% taxon$accepted_no) / length(unique.taxa)
-print(paste('% pbdb taxon ids in macrostrat that are reflected in the pbdb: ', 
-            round(taxon.percent, digits = 2)))
-
-
-# grab collection information from pbdb
-fossil.url <- paste0('https://paleobiodb.org/data1.2/colls/list.txt?coll_id=',
-                     paste0(ord$cltn_id, collapse = ','), 
-                     '&show=full')
-cltn <- read.csv(fossil.url, stringsAsFactors = FALSE)
-
-cltn.percent <- sum(unique(ord$cltn_id) %in% unique(cltn$collection_no)) /
-  length(unique(ord$cltn_id))
-print(paste('% pbdb collection ids in macrostrat that are reflected in the pbdb:', 
-            round(cltn.percent, digits = 2)))
-
-
-
-# do collections at least make sense temporally?
-# lets look just in macrostrat
-times <- strat.ord[, c('unit_id', 't_age', 'b_age')]
-ff <- ord[, c('cltn_id', 'unit_id', 't_age', 'b_age')]
-ffunit <- times[times$unit_id %in% ff$unit_id, ]
-
-exu <- ffunit[ffunit$unit_id == ff$unit_id[1], ]
-ex <- ff[ff$unit_id == ff$unit_id[1], ]
-
-# macrostrat collection age is identical to macrostrat unit age
-#   no superposition information recorded here
-macro.compare <- all(apply(ex[, c('t_age', 'b_age')], 1, function(x) 
-                           all(x == exu[, c('t_age', 'b_age')])))
-
-# compare macrostrat collection info to pbdb collection information
-cc <- cltn[, c('collection_no', 'min_ma', 'max_ma')]
-ffcc <- ff[ff$cltn_id %in% cc$collection_no, ]
-
-exc <- ffcc[ffcc$cltn_id == cc$collection_no[1], ]  # based on macrostat
-ex <- cc[cc$collection_no == cc$collection_no[1], ]  # based on pbdb
-
-print('collection information from macrostrat:')
-print(exc)
-print('collection information from pbdb:')
-print(ex)
